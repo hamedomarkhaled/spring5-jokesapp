9d33ed81ff861c897da0509dcbbabd94980317e3
Omar Hamed <hamedomarkhaled@gmail.com>
2020-09-12 15:43:46 +0200

    xml configuration example

--- src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
@@ -3,9 +3,11 @@ package guru.springframework.joke.jokeapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"guru.springframework"})
+@ImportResource("classpath:chuck-norris.xml")
 public class JokeappApplication {
 
     public static void main(String[] args) {
